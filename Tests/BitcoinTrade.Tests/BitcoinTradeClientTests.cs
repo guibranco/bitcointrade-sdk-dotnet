2c1e0d5c8a341635686e6247236de39fb3a0fe21
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-07-06 22:17:42 -0300

    Initial commit

---
+++ Tests/BitcoinTrade.Tests/BitcoinTradeClientTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace BitcoinTrade.Tests
+{
+    public class BitcoinTradeClientTests
+    {
+
+        [Fact]
+        public void ValidateInitial()
+        {
+            Assert.True(true);
+        }
+    }
+}
